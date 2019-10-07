ea177e0eb3f31012e2cb06d4fc963920fa76d2e9
nigelgilbert <nigelcodes@gmail.com>
2019-10-07 17:03:58 -0500

    Re-testing for interactives instructions

--- src/index.js
+++ src/index.js
@@ -6,7 +6,21 @@ import Masthead from "./masthead";
 import Nav from "./nav";
 import Footer from "./footer";
 
+const appendMarkup = (el, markup) => {
+  try {
+    const div = document.createElement("div");
+    div.innerHTML = markup;
+    document.getElementById(el).appendChild(div);
+  } catch (err) {
+    // do nothing
+  }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  // appendMarkup("nav", Masthead());
+  // appendMarkup("nav", Nav());
+  // appendMarkup("footer", Footer());
+  
   const masthead = document.createElement("div");
   masthead.innerHTML = Masthead();
   document.getElementById("nav").appendChild(masthead);
